bf95175f7d343538d82b5d812be4a4230afe04ac
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2025-01-06 10:36:54 -0500

    Add encryption with transit secrets engine

--- src/main/java/com/example/workshop_spring_vault/PaymentController.java
+++ src/main/java/com/example/workshop_spring_vault/PaymentController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.sql.DataSource;
 import java.util.Collection;
-import java.util.UUID;
+import java.util.SequencedCollection;
+
+import static java.lang.Long.valueOf;
 
 @Controller
 @ResponseBody
@@ -23,9 +25,8 @@ class PaymentController {
         this.vaultTransit = new VaultTransit(appProperties, vaultTemplate);
     }
 
-    @GetMapping("/paymentcard/{id}")
-    Collection<Payment> getPaymentByID(@PathVariable String id) {
-        return this.db
+    private SequencedCollection<Payment> getPaymentById(JdbcClient db, String id) {
+        return db
                 .sql(String.format("SELECT * FROM payment_card WHERE id = '%s'", id))
                 .query((rs, rowNum) -> new Payment(
                         rs.getLong("id"),
@@ -39,32 +40,28 @@ class PaymentController {
                 )).list();
     }
 
+    @GetMapping("/paymentcard/{id}")
+    Collection<Payment> getPaymentByID(@PathVariable String id) {
+        return getPaymentById(this.db, id);
+    }
+
     @PostMapping(path = "/paymentcard",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     Collection<Payment> createPayment(@RequestBody Payment request) {
-        var id = UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
         var statement = String.format(
                 "INSERT INTO payment_card(user_id, name, number, expiry, cv3) "
-                        + "VALUES('%s', '%s', '%s', '%s', '%s')",
+                        + "VALUES('%s', '%s', '%s', '%s', '%s') "
+                        + "RETURNING id",
                 request.userId(),
                 request.name(),
-                request.number(),
+                vaultTransit.encrypt(request.number()),
                 request.expiry(),
                 request.cv3());
-        this.db.sql(statement).update();
+        var id = this.db.sql(statement).query((rs, rowNum) -> valueOf(
+                rs.getLong("id")
+        )).list();
 
-        return this.db
-                .sql(String.format("SELECT * FROM payment_card WHERE id = '%s'", request.id()))
-                .query((rs, rowNum) -> new Payment(
-                        rs.getLong("id"),
-                        rs.getLong("user_id"),
-                        rs.getString("name"),
-                        rs.getString("number").startsWith("vault") ?
-                                vaultTransit.decrypt(rs.getString("number")) :
-                                rs.getString("number"),
-                        rs.getString("expiry"),
-                        rs.getString("cv3")
-                )).list();
+        return getPaymentById(this.db, id.get(0).toString());
     }
 }
